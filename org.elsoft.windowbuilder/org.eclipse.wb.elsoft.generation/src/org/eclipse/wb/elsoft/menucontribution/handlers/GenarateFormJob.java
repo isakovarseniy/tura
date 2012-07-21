8955fbfe6076b3f3ad9bfd7dd9665011aa42b287
Arseniy Isakov <isakovarseniy@gmail.com>
2012-07-20 20:50:55 -0400

    Forms generation performance issue

--- org.elsoft.windowbuilder/org.eclipse.wb.elsoft.generation/src/org/eclipse/wb/elsoft/menucontribution/handlers/GenarateFormJob.java
+++ org.elsoft.windowbuilder/org.eclipse.wb.elsoft.generation/src/org/eclipse/wb/elsoft/menucontribution/handlers/GenarateFormJob.java
@@ -15,15 +15,13 @@
  ******************************************************************************/
 package org.eclipse.wb.elsoft.menucontribution.handlers;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.util.HashMap;
 
 import org.eclipse.ui.console.MessageConsole;
 import org.eclipse.ui.console.MessageConsoleStream;
-import org.eclipse.wb.elsoft.propertyeditor.Activator;
-import org.elsoft.platform.metamodel.MetamodelPlatformLevel;
-import org.elsoft.platform.metamodel.suite.ApplicationHandler;
-import org.elsoft.platform.metamodel.suite.RecipeHandler;
 
 public class GenarateFormJob implements Runnable {
 
@@ -46,34 +44,47 @@ public class GenarateFormJob implements Runnable {
 		System.setErr(new PrintStream(out));
 
 		try {
+			Runtime runtime = Runtime.getRuntime();
 
-			ApplicationHandler ah = Activator.rf.getRoot().clean()
-					.searchString("domainName", domain).seek()
-					.getFunctionalDomain().clean()
-					.searchString("functionalDomainName", functionalDomain)
-					.seek().getApplicationHandler().clean()
-					.searchString("applicationName", application).seek();
+			System.out
+					.println("mvn -f /home/platform/GITRepo/tura/platform/metamodel/artifact-generator/pom.xml exec:java -Dexec.args=\""
+							+ domain
+							+ " "
+							+ functionalDomain
+							+ " "
+							+ application
+							+ " '"
+							+ infrastructure
+							+ "'"
+							+ " '"
+							+ recipe + "'" + " UI_CONTAINER UI_CONTAINER\"");
 
-			HashMap<String, Object> infrastructureContext = ah
-					.getInfrastructureLayer().clean()
-					.searchString("zoneName", infrastructure).seek()
-					.loadProperties(Activator.rf);
+			String[] cmd = new String[] {
+					"mvn",
+					"-f",
+					"/home/platform/GITRepo/tura/platform/metamodel/artifact-generator/pom.xml",
+					"exec:java",
+					"-Dexec.args=" + domain + " " + functionalDomain + " "
+							+ application + " '" + infrastructure + "'" + " '"
+							+ recipe + "'" + " UI_CONTAINER UI_CONTAINER\"" };
 
-			RecipeHandler rh = ah.getRecipeHandler()
-					.searchString("recipeName", recipe).seek();
+			Process process = runtime.exec(cmd);
 
-			rh.buildRecipe(Activator.rf, infrastructureContext,
-					MetamodelPlatformLevel.UI_CONTAINER,
-					MetamodelPlatformLevel.UI_CONTAINER, domain,
-					functionalDomain, application);
+			InputStream is = process.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			String line;
+
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			System.setOut(oldOut);
 			System.setErr(olderr);
+
 		}
-
 	}
-
 }
