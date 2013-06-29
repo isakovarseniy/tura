15f8e5fafa0fd7de3743fa73ec77dba41c86ef09
Arseniy Isakov <isakovarseniy@gmail.com>
2013-06-29 15:12:13 -0400

    OCI query added to property

--- gmfeditor/org.tura.metamodel.commons/src/org/metamodel/tura/ui/properties/sections/TypeNamePropertySection.java
+++ gmfeditor/org.tura.metamodel.commons/src/org/metamodel/tura/ui/properties/sections/TypeNamePropertySection.java
@@ -1,6 +1,7 @@
 package org.metamodel.tura.ui.properties.sections;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.EventObject;
 import java.util.Iterator;
 
@@ -8,17 +9,24 @@ import org.eclipse.emf.common.command.BasicCommandStack;
 import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.common.command.CommandStackListener;
 import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.edit.command.SetCommand;
 import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.emf.validation.internal.modeled.model.validation.Constraint;
 import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditor;
 import org.eclipse.gmf.runtime.notation.Diagram;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ocl.OCL;
+import org.eclipse.ocl.ecore.EcoreEnvironmentFactory;
+import org.eclipse.ocl.expressions.OCLExpression;
+import org.eclipse.ocl.helper.OCLHelper;
 import org.eclipse.ui.IWorkbenchPart;
 import org.tura.metamodel.commons.types.impl.RefTypeImpl;
 
+import tura.TuraPackage;
 import tura.impl.PackageImpl;
 import tura.impl.TypesImpl;
-import typedefinition.TypeElement;
+import typedefinition.Type;
 import typedefinition.TypedefinitionPackage;
 import typedefinition.TypePointer;
 
@@ -90,45 +98,55 @@ public class TypeNamePropertySection extends AbstractEnumerationPropertySection 
 				((TypePointer) eObject).getType().getTypeName());
 	}
 
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	protected String[] getEnumerationFeatureValues() {
 
 		if (values == null) {
 			values = new ArrayList<String>();
+
 			Diagram diagram = (Diagram) editPart.getRoot().getContents()
 					.getModel();
 			PackageImpl pckg = (PackageImpl) diagram.getElement();
 			TypesImpl types = (TypesImpl) pckg.eContainer();
-     
-			if (  ((TypePointer) eObject).getType() == null )  return new String[]{};
-			
-			if (!(((TypePointer) eObject).getType().getPackageName())
-					.equals("Primitives")) {
-				for (Iterator<tura.Package> i = types.getPackages().iterator(); i
-						.hasNext();) {
-					tura.Package p = i.next();
-					if ((p.getName() != null)
-							&& (((TypePointer) eObject).getType() != null)) {
-						if (p.getName().equals(
-								((TypePointer) eObject).getType()
-										.getPackageName())) {
-							for (Iterator<TypeElement> j = p.getTypes()
-									.iterator(); j.hasNext();) {
-								TypeElement te = j.next();
-								if ((te instanceof typedefinition.Type)
-										&& (((typedefinition.Type) te)
-												.getName() != null))
-									values.add(((typedefinition.Type) te)
-											.getName());
-							}
-						}
+
+			if ((((TypePointer) eObject).getType() == null)
+					|| ((TypePointer) eObject).getType().getPackageName() == null)
+				return new String[] {};
+
+			OCL ocl = OCL.newInstance(EcoreEnvironmentFactory.INSTANCE);
+			OCLHelper<EClassifier, ?, ?, Constraint> helper = ocl
+					.createOCLHelper();
+			helper.setContext(TuraPackage.eINSTANCE.getEClassifier("Types"));
+
+			try {
+				if ((((TypePointer) eObject).getType().getPackageName())
+						.equals("Primitives")) {
+					OCLExpression<EClassifier> query = helper
+							.createQuery("self.primitives");
+					Collection<tura.Primitive> map = (Collection<tura.Primitive>) ocl
+							.evaluate(types, query);
+					for (Iterator<tura.Primitive> i = map.iterator(); i
+							.hasNext();) {
+						tura.Primitive p = i.next();
+						values.add(p.getName());
 					}
+				} else {
+					OCLExpression<EClassifier> query = helper
+							.createQuery("self.packages->select(r|r.name='"
+									+ ((TypePointer) eObject).getType()
+											.getPackageName() + "').types");
+					Collection<typedefinition.Type> map = (Collection<Type>) ocl.evaluate(
+							types, query);
+
+					for (Iterator<typedefinition.Type> i = map.iterator(); i
+							.hasNext();) {
+						typedefinition.Type p = i.next();
+						values.add(p.getName());
+					}
+
 				}
-			} else {
-				for (Iterator<tura.Primitive> i = types.getPrimitives()
-						.iterator(); i.hasNext();) {
-					tura.Primitive p = i.next();
-					values.add(p.getName());
-				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
 
