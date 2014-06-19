752aa492662faec79449727a7d7a839b5efccb0c
Arseniy Isakov <isakovarseniy@gmail.com>
2014-06-19 10:24:37 -0400

    Metamodel markers

---
+++ gmfeditor/org.tura.metamodel.commons.markers/src/org/tura/metamodel/commons/markers/ProblemsMarkersView.java
@@ -0,0 +1,9 @@
+package org.tura.metamodel.commons.markers;
+
+import org.eclipse.ui.views.markers.MarkerSupportView;
+
+public class ProblemsMarkersView extends MarkerSupportView {
+	public ProblemsMarkersView() {
+		super("org.tura.metamodel.commons.markers.TuraMarkerGenerator");
+	}
+}
