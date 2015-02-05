47dc5b58838275cdacce13d56a719fd3e9b3fd22
Arseniy Isakov <isakovarseniy@gmail.com>
2015-02-05 01:06:32 -0500

    PreFormTrigger and form parameters

---
+++ platform/datacontrol/core/src/main/java/org/tura/platform/datacontrol/command/PreFormTrigger.java
@@ -0,0 +1,7 @@
+package org.tura.platform.datacontrol.command;
+
+import org.tura.platform.datacontrol.commons.TuraException;
+
+public interface PreFormTrigger {
+	public void execute() throws TuraException;
+}
