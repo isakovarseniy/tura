828506119cbf524318b2fc6205dd9dd803c976cc
Arseniy Isakov <isakovarseniy@gmail.com>
2015-02-08 12:10:22 -0500

    PreForm trigger with reading of form parameters

---
+++ examples/hrform-triggers/src/main/java/org/tura/example/ui/hrcontroller/triggers/PreFormTrigger.java
@@ -0,0 +1,28 @@
+package org.tura.example.ui.hrcontroller.triggers;
+
+import javax.annotation.Priority;
+import javax.enterprise.inject.Alternative;
+import javax.faces.context.FacesContext;
+
+import org.tura.platform.datacontrol.command.FactoryInitializeTrigger;
+import org.tura.platform.datacontrol.commons.Reflection;
+import org.tura.platform.datacontrol.commons.TuraException;
+
+@Alternative
+@Priority(10)
+public class PreFormTrigger implements FactoryInitializeTrigger {
+
+	@Override
+	public void execute(Object obj) throws TuraException {
+		try {
+			String param1 = FacesContext.getCurrentInstance()
+					.getExternalContext().getRequestParameterMap()
+					.get("param1");
+			Reflection.call(obj, "setVar1", param1);
+			System.out.println("NA");
+		} catch (Exception e) {
+            throw new TuraException(e);
+		}
+	}
+
+}
