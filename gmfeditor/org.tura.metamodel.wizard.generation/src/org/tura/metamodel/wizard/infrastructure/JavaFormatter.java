c68c66867ef591acbfd055d03c60795d6ee63c40
Arseniy Isakov <isakovarseniy@gmail.com>
2015-07-02 19:02:18 -0400

    Remove output log from hybridlab-beautifier

--- gmfeditor/org.tura.metamodel.wizard.generation/src/org/tura/metamodel/wizard/infrastructure/JavaFormatter.java
+++ gmfeditor/org.tura.metamodel.wizard.generation/src/org/tura/metamodel/wizard/infrastructure/JavaFormatter.java
@@ -4,6 +4,8 @@ import org.eclipse.epsilon.egl.formatter.Formatter;
 import org.hybridlabs.source.beautifier.CharacterSequence;
 import org.hybridlabs.source.beautifier.JavaImportBeautifierImpl;
 
+import ch.qos.logback.classic.Level;
+
 public class JavaFormatter implements Formatter {
 
 	@Override
@@ -13,6 +15,9 @@ public class JavaFormatter implements Formatter {
 		beautifier.setOrganizeImports(true);
 		beautifier.setFormat(true);
 		
+		ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME);
+	    root.setLevel(Level.OFF);
+				
         CharacterSequence sequence = new CharacterSequence(text);
         beautifier.beautify(sequence);
         
