6095017d226756a27fe56fe7016a72e4503cb3a9
Arseniy Isakov <isakovarseniy@gmail.com>
2015-08-16 23:06:36 -0400

    Packaging

--- gmfeditor/plugins/org.tura.metamodel.commons.properties/src/org/tura/metamodel/commons/properties/selections/AbstractTextPropertySection.java
+++ gmfeditor/plugins/org.tura.metamodel.commons.properties/src/org/tura/metamodel/commons/properties/selections/AbstractTextPropertySection.java
@@ -1,12 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+ * Tura - application generation platform
+ *
+ * Copyright (c) 2012, 2015, Arseniy Isakov
+ *  
+ * This project includes software developed by Arseniy Isakov
+ * http://sourceforge.net/p/tura/wiki/Home/
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors:
- *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package org.tura.metamodel.commons.properties.selections;
 
