81e9b3f2a454850a38e721c3f54e3a8186bbe81d
Arseniy Isakov <isakovarseniy@gmail.com>
2016-08-18 19:03:33 -0400

    Sirius editor

---
+++ sirius/plugins/org.tura.metamodel.sirius.properties/src/org/tura/metamodel/sirius/properties/selections/grid/impl/FlexFieldCategorizedPropertySelection.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Tura - application generation platform
+ *
+ * Copyright (c) 2012, 2015, Arseniy Isakov
+ *  
+ * This project includes software developed by Arseniy Isakov
+ * http://sourceforge.net/p/tura/wiki/Home/
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.tura.metamodel.sirius.properties.selections.grid.impl;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
+import org.tura.metamodel.sirius.properties.selections.events.Bus;
+import org.tura.metamodel.sirius.properties.selections.events.Event;
+import org.tura.metamodel.sirius.properties.selections.events.EventListener;
+import org.tura.metamodel.sirius.properties.selections.events.FlexFieldChangeEvent;
+
+import form.FlexField;
+import form.FlexFields;
+
+public class FlexFieldCategorizedPropertySelection extends CategorizedPropertySelection {
+
+	private FlexField model;
+	private SelectionListener listener = new SelectionListener();
+
+	@Override
+	public void createControls(Composite parent, final TabbedPropertySheetPage aTabbedPropertySheetPage) {
+		super.createControls(parent, aTabbedPropertySheetPage);
+		Bus.getInstance().addEventListner(listener);
+	}
+
+	@Override
+	public EObject getModel() {
+		if (model == null) {
+			tableViewer.setInput(null);
+			FlexFields fields = (FlexFields) model;
+			if (fields == null || !fields.getFields().iterator().hasNext())
+				return null;
+			return fields.getFields().iterator().next();
+		}
+		return model;
+	}
+
+
+	private class SelectionListener implements EventListener {
+
+		@Override
+		public void handleEvent(Event event) {
+			if (event instanceof FlexFieldChangeEvent) {
+				model = (FlexField) ((FlexFieldChangeEvent)event).getModel();
+				ds.cleanList();
+				ds.getRows();
+				refresh();
+			}
+		}
+	}
+
+}
