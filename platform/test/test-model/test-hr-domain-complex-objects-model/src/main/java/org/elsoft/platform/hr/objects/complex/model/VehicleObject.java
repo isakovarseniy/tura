bdcf569bc869db33df5c529abd863421c2b4c511
Arseniy Isakov <isakovarseniy@gmail.com>
2016-11-11 11:12:21 -0500

    reorganizing of sources

--- platform/test/test-model/test-hr-domain-complex-objects-model/src/main/java/org/elsoft/platform/hr/objects/complex/model/VehicleObject.java
+++
@@ -1,6 +0,0 @@
-package org.elsoft.platform.hr.objects.complex.model;
-
-import org.elsoft.platform.hr.objects.simple.model.Vehicle;
-
-public class VehicleObject extends Vehicle {
-}
--- platform/test/test-model/test-hr-domain-objects-interface/src/main/java/org/elsoft/platform/hr/objects/StateDAO.java
+++
@@ -1,9 +0,0 @@
-package org.elsoft.platform.hr.objects;
-
-import org.tura.platform.object.TuraObject;
-
-public interface StateDAO extends TuraObject {
-    public void setName(String name);
-
-    public String getName();
-}
