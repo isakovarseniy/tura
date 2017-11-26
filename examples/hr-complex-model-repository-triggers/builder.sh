fa6f1df7b0708ea7a5746c48db2c8053a7587556
Arseniy Isakov <isakovarseniy@gmail.com>
2017-11-25 20:54:57 -0500

    expose repository through typescript

--- examples/hr-complex-model-repository-triggers/builder.sh
+++ examples/hr-complex-model-repository-triggers/builder.sh
@@ -3,5 +3,5 @@
 
 export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 
-cd "/Users/arseniy/MyPrj/git/tura.4.3.0/examples/hr-complex-model-repository-triggers"
+cd "/Users/arseniy/MyPrj/git/tura.4.2.2/examples/hr-complex-model-repository-triggers"
 "/Users/arseniy/tools/apache-maven-3.2.2"/bin/mvn -B clean install
--- examples/hrform-triggers/builder.sh
+++ examples/hrform-triggers/builder.sh
@@ -3,5 +3,5 @@
 
 export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 
-cd "/Users/arseniy/MyPrj/git/tura.4.3.0/examples/hrform-triggers"
+cd "/Users/arseniy/MyPrj/git/tura.4.2.2/examples/hrform-triggers"
 "/Users/arseniy/tools/apache-maven-3.2.2"/bin/mvn -B clean install
