bf39f3740785ad067d5830f437a5a58bcf1deda9
Arseniy Isakov <isakovarseniy@gmail.com>
2018-05-08 22:03:13 -0400

    Internal relationship

--- platform/repository/commons/src/main/java/org/tura/platform/repository/Mapper.java
+++ platform/repository/commons/src/main/java/org/tura/platform/repository/Mapper.java
@@ -4,7 +4,6 @@ public interface Mapper {
 
 	Object getPrimaryKey(Object persistenceObject);
 	Object copyFromPersistence2Repository(Object persistenceObject, Object repositoryObject);
-	Object copyFromRepository2Persistence(Object repositoryObject, Object persistenceObject);
 
 }
 
