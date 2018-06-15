c058a2fc5ef3a6b75093eb2b3d0bd7683ea85171
Arseniy Isakov <isakovarseniy@gmail.com>
2018-06-15 19:45:44 -0400

    Restore Test

--- examples/hr-manager/hrform-triggers/builder.sh
+++ examples/hr-manager/hrform-triggers/builder.sh
@@ -3,5 +3,5 @@
 
 export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
 
-cd "/Users/arseniy/MyPrj/git/tura/examples/hrresources"
+cd "/Users/arseniy/MyPrj/git/tura/examples/hr-manager/hrform-triggers"
 "/Users/arseniy/tools/apache-maven-3.2.2"/bin/mvn -B clean install
