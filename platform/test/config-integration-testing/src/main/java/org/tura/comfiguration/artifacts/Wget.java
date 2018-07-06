3b5d87ad59fab1938f597dcb484aaf4032be441a
Arseniy Isakov <isakovarseniy@gmail.com>
2018-07-05 23:20:50 -0400

    Progress bar for wget

--- platform/test/config-integration-testing/src/main/java/org/tura/comfiguration/artifacts/Wget.java
+++ platform/test/config-integration-testing/src/main/java/org/tura/comfiguration/artifacts/Wget.java
@@ -29,44 +29,57 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+
+import me.tongfei.progressbar.ProgressBar;
 
 public class Wget {
 
-  public static WgetStatus wGet(String saveAsFile, String urlOfFile) {
-    InputStream httpIn = null;
-    OutputStream fileOutput = null;
-    OutputStream bufferedOut = null;
-    try {
-      // check the http connection before we do anything to the fs
-      httpIn = new BufferedInputStream(new URL(urlOfFile).openStream());
-      // prep saving the file
-      fileOutput = new FileOutputStream(saveAsFile);
-      bufferedOut = new BufferedOutputStream(fileOutput, 1024);
-      byte data[] = new byte[1024];
-      boolean fileComplete = false;
-      int count = 0;
-      while (!fileComplete) {
-        count = httpIn.read(data, 0, 1024);
-        if (count <= 0) {
-          fileComplete = true;
-        } else {
-          bufferedOut.write(data, 0, count);
-        }
-      }
-    } catch (MalformedURLException e) {
-      return WgetStatus.MalformedUrl;
-    } catch (IOException e) {
-      return WgetStatus.IoException;
-    } finally {
-      try {
-        bufferedOut.close();
-        fileOutput.close();
-        httpIn.close();
-      } catch (IOException e) {
-        return WgetStatus.UnableToCloseOutputStream;
-      }
-    }
-    return WgetStatus.Success;
-  }
+	public static WgetStatus wGet(String saveAsFile, String urlOfFile) {
+		InputStream httpIn = null;
+		OutputStream fileOutput = null;
+		OutputStream bufferedOut = null;
+		try {
+			// check the http connection before we do anything to the fs
+
+			URL url = new URL(urlOfFile);
+			URLConnection connection = url.openConnection();
+			int size = connection.getContentLength();
+
+			httpIn = new BufferedInputStream(connection.getInputStream());
+
+			// prep saving the file
+			fileOutput = new FileOutputStream(saveAsFile);
+			bufferedOut = new BufferedOutputStream(fileOutput, 1024);
+			byte data[] = new byte[1024];
+			boolean fileComplete = false;
+			int count = 0;
+			try (ProgressBar pb = new ProgressBar("Test", size)) {
+
+				while (!fileComplete) {
+					count = httpIn.read(data, 0, 1024);
+					if (count <= 0) {
+						fileComplete = true;
+					} else {
+						bufferedOut.write(data, 0, count);
+					}
+					pb.stepBy(count);
+				}
+			}
+		} catch (MalformedURLException e) {
+			return WgetStatus.MalformedUrl;
+		} catch (IOException e) {
+			return WgetStatus.IoException;
+		} finally {
+			try {
+				bufferedOut.close();
+				fileOutput.close();
+				httpIn.close();
+			} catch (IOException e) {
+				return WgetStatus.UnableToCloseOutputStream;
+			}
+		}
+		return WgetStatus.Success;
+	}
 
 }
