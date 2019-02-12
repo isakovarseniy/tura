8a251ed5df45dce28a4258958b20e6af2d05a785
Arseniy Isakov <isakovarseniy@gmail.com>
2019-02-12 00:03:45 -0500

    gogo shell configuration

--- examples/sales-analyzer/sales-analyzer/builderTesting.sh
+++ examples/sales-analyzer/sales-analyzer/builderTesting.sh
@@ -3,5 +3,5 @@
 
 export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 
-cd "/Users/arseniy/MyPrj/git/tura/processor/../examples/sales-analyzer/sales-analyzer/"
-"/Users/arseniy/.tura/resources/apache-maven"/bin/mvn -B clean install -P Testing
+cd "/Users/arseniy/MyPrj/git/tura/examples/sales-analyzer/sales-analyzer/"
+"${HOME}/.tura/resources/apache-maven"/bin/mvn -B clean install -P Testing
