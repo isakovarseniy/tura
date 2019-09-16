466ae9a4e13f5473577d78ab5543e583f459e978
Arseniy A. Isakov <isakovarseniy@gmail.com>
2019-09-15 20:02:58 -0400

    update license for processor

--- processor/src/main/java/org/apache/felix/gogo/jline/ext/ProcessorExtension.java
+++ processor/src/main/java/org/apache/felix/gogo/jline/ext/ProcessorExtension.java
@@ -6,18 +6,10 @@
  * This project includes software developed by Arseniy Isakov
  * https://github.com/isakovarseniy/tura
  *
- * Licensed under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 1.0
+ * which is available at https://www.eclipse.org/legal/epl-v10.html
  *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
  */
 package org.apache.felix.gogo.jline.ext;
 
